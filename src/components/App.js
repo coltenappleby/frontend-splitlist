9e9b63e6be53922d7cb81339c6e78f193ed28049
coltenappleby <colten.appleby@gmail.com>
2021-09-11 00:31:39 -0400

    still relearning react. having issues with async, useeffect, and a reload

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+//   Link
 } from "react-router-dom"; // npm install react-router-dom
 import ListIndex from './ListIndex.js';
 import ListShow from './ListShow.js'
--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -1,23 +1,32 @@
 import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom'
 
 
 // need to set up routes
 
 function ListShow() {
 
-    const[listInfo, setListInfo] = useState()
+    const[listInfo, setListInfo] = useState('')
+    let { id } = useParams()
 
+    // console.log(id) // id works
+ 
     useEffect(() => {
-        fetch('http://localhost:3000/lists/[:id]')
+        fetch(`http://localhost:3000//lists/${id}`)
         .then(resp => resp.json())
-        .then(setListInfo)
+        .then((listInfo) => {
+            console.log(listInfo)
+            setListInfo(setListInfo)
+        })
       }, [])
 
     console.log(listInfo)
 
     return(
         <div>
-            {listInfo.name}
+            {listInfo !== '' ? '' : (
+                `${listInfo.id}`
+            )}
         </div>
     )
 
