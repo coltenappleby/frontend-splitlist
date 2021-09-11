c60dff23bd8aed124c6d11d23069664bc7b5d07b
coltenappleby <colten.appleby@gmail.com>
2021-09-11 16:38:39 -0400

    fixed useState

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -6,27 +6,32 @@ import { useParams } from 'react-router-dom'
 
 function ListShow() {
 
-    const[listInfo, setListInfo] = useState('')
+    const[listInfo, setListInfo] = useState('1')
     let { id } = useParams()
 
     // console.log(id) // id works
+
+    console.log(listInfo)
  
     useEffect(() => {
         fetch(`http://localhost:3000//lists/${id}`)
         .then(resp => resp.json())
         .then((listInfo) => {
             console.log(listInfo)
-            setListInfo(setListInfo)
+            setListInfo(listInfo)
         })
-      }, [])
+      }, [id])
 
     console.log(listInfo)
 
     return(
         <div>
-            {listInfo !== '' ? '' : (
-                `${listInfo.id}`
-            )}
+            {listInfo === '1' ? '' : 
+            <div> 
+                <p> List Information: </p>
+                <p> {listInfo.id} </p>
+            </div>
+            }
         </div>
     )
 
