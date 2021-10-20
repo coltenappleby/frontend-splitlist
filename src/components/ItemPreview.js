df1b88ab0a2f656dc574fd08b23730c5aa4baedc
coltenappleby <colten.appleby@gmail.com>
2021-10-20 00:22:27 -0400

    notes on how to chnage the items

--- src/components/ItemPreview.js
+++ src/components/ItemPreview.js
@@ -8,24 +8,35 @@ function ItemPreview({id, name, notes, location, completed}) {
     // When I click Edit, itemPreview information disapears and a card opens in its space 
     // with more information and the ability to change its information
 
+
+    // new set up
+    // Item Card lives on List
+    // Item Show presents data
+    // When Edit is selected, Item Edit is shown
+    // Need to figure out how to only let a user edit one card at a time.
+    // If handle change and handle submit live on ItemEdit, will information change on the show?
+    // I assume we would need a re-render in that case. Unsure how to push that data back up.
+    // Maybe we have some state that lives in the ListShow that keeps track what item is being editted at a time
+    // and another piece of state that can trigger a useeffect re-render to render a card 
+
     const [showEdit, setShowEdit] = useState(false)
 
     function handleChange(e) {
         setShowEdit(!showEdit)
     }
 
-    function handleSubmit(e){
-        e.preventDefault()
-        fetch(`http://localhost:3000/items/${id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(formData)
+    // function handleSubmit(e){
+    //     e.preventDefault()
+    //     fetch(`http://localhost:3000/items/${id}`, {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+    //         },
+    //         body: JSON.stringify(formData)
 
-        }
-    }
+    //     }
+    // }}
 
     function handleSubmit(e){
         e.preventDefault() 
